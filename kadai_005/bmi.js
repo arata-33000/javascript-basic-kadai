24f71d7ee23cff1964df2ed827572967d4d4311f
arata-33000 <arata33000@gmail.com>
2025-08-30 10:52:27 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+// 変数の宣言
+  let weight;                         // 体重（kg）
+  let height;                         // 身長（m）
+  let bmi;                            // bmi
+// 変数の宣言・値の代入
+  weight = 68;                     // 体重（kg）
+  height = 1.7;                    // 身長（m）
+  bmi = weight / (height * height);// bmi計算式
+// コンソールへの出力
+  console.log(bmi);
